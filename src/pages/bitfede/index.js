fcca6b225897f4952823aa065285a7c8bc67593a
Martin Di Palma <tincho.dipalma@gmail.com>
2020-01-06 10:04:11 -0300

    bitfede

---
+++ src/pages/bitfede/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './bitfede.css';
+
+const BitFede = () => (
+  <div className="BitFede">
+    <h1>> Fede</h1>
+    <p>> Supongo que todavía no sabes quién soy (?.</p>
+    <p>> Es increíble lo que aprendí con vos durante todo este tiempo, siento que gran parte de mi crecimiento profesional es por trabajar con vos diariamente.</p>
+    <p>> Estoy muy seguro que todos sentimos lo mismo, y al mismo tiempo creo que vos también te das cuenta eso.</p>
+    <p>> No dejes nunca de abrir cabezas, de debatir soluciones, de rearmar los planes para mejorar y levantar todo lo que venga.</p>
+    <p>> En este año peronista, te deseo un 2020 genial!</p>
+  </div>
+);
+
+export default BitFede;
